c509ec306379c6b2115fc60f17a88d252685610d
Polina <phlejnas@mail.ru>
2023-02-07 11:27:02 +0300

    Creating table html

--- simpsons/simpsons_app/models.py
+++ simpsons/simpsons_app/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class Simpsons(models.Model):
+    simpson_id = models.AutoField(primary_key=True, default=None)
     name = models.CharField(max_length=225)
     picture = models.ImageField(upload_to='images/', default=None)
